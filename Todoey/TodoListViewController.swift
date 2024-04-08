4cbc557671aaf972c1eedbf9063274b2874e7fef
jasonfangmagic <jasonfangmagic@gmail.com>
2024-04-08 18:41:57 -0400

    add coredata

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -7,22 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
         
-    var itemArray = ["get up", "sleep", "work"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+//    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        if let items  = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray = items
-        }
-    }
+                    
+      loadItems()
 
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -32,19 +33,36 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark: .none
+        
+        if itemArray[indexPath.row].done == true {
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        
+        tableView.reloadData()
+        
+        
         return cell
     }
     
+
+    
+ 
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
         
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+       
+        
+        saveItems()
+        
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -57,12 +75,14 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
             
-            self.itemArray.append(textField.text!)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.tableView.reloadData()
+            let newItem = Item(context: self.context)
+                
+            newItem.title = textField.text!
+            newItem.done = false
             
+            self.itemArray.append(newItem)
             
         }
         
@@ -75,6 +95,49 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated:  true, completion: nil)
+        
+    }
+    
+    func saveItems(){
+   
+        do{
+            
+            try context.save()
+            
+        } catch{
+           print("error")
+        }
+        
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+//        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("error")
+        }
+    }
+    
+    
+   
+    
+}
+
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+    
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+        loadItems(with: request)
         
     }
     
